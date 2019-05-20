8a45b3d54e1a4eae3cfbd2ef3528cc3f73547404
Amir Honarpour <amir@Amirs-MacBook-Air.local>
2019-05-20 16:56:31 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,72 @@
+# Serverless Export Outputs
+
+> A Serverless plugin for exporting AWS stack outputs to a file.
+
+By default, this plugin exports all stack outputs to a `.env` file in the root of the project, and formats all keys as `OutputKeyName → REACT_APP_OUTPUT_KEY_NAME`, which allows Create React App to pick them up as `process.env` variables. You can override these in steps 3 and 4 of setup and use it for any other purpose.
+
+## Setup
+
+1. Add dependency to `package.json`:
+
+   ```sh
+   npm add -D serverless-export-outputs
+   ```
+
+   or
+
+   ```sh
+   yarn add -D serverless-export-outputs
+   ```
+
+2. Add the plugin to `serverless.yml` file:
+
+   ```yaml
+   plugins:
+     - serverless-export-outputs
+   ```
+
+3. Choose which outputs get exported (optional):
+
+   ```yaml
+   custom:
+     exportOutputs: # if not provided, all outputs are exported
+       - OutputKeyName
+       - AnotherOutputKeyName
+       - CustomOutput: value # add custom key/value to exports
+
+   Outputs:
+     OutputKeyName:
+       Value: Lorem ipsum
+     AnotherOutputKeyName:
+       Value: Lorem ipsum
+     ThisOutputWontExport:
+       Value: Lorem ipsum
+   ```
+
+4. Override defaults (optional):
+
+   ```yaml
+   custom:
+     exportOutputs:
+       include: # if not provided, all outputs are exported
+         - OutputKeyName
+         - AnotherOutputKeyName
+         - CustomOutput: value # add custom key/value to exports
+       handler: scripts/env.handler # script to process outputs
+       output:
+         file: ./.env # file path and name relative to root
+         format: toml # toml, yaml/yml, json
+   ```
+
+   Handler at `scripts/env.js`:
+
+   ```js
+   function handler(outputs, serverless, options) {
+     console.log({ outputs });
+     return outputs;
+   }
+
+   module.exports = { handler };
+   ```
+
+   The handler above overrides/removes prefixing for Create React App.
---
+++ index.js
@@ -0,0 +1,146 @@
+const fs = require('fs');
+const tomlify = require('tomlify-j0.4');
+const yamljs = require('yamljs');
+
+const defaultPrefix = 'REACT_APP_';
+const defaultFile = './.env';
+const defaultFormat = 'toml';
+
+const camelToSnakeCase = string =>
+  string.replace(
+    /[A-Z]/g,
+    (letter, index) => `${index > 0 ? '_' : ''}${letter}`,
+  );
+
+const defaultHandler = outputs => {
+  const processedOutputs = {};
+
+  Object.keys(outputs).forEach(key => {
+    const newKey = `${defaultPrefix}${camelToSnakeCase(key)}`.toUpperCase();
+    processedOutputs[newKey] = outputs[key];
+  });
+
+  return processedOutputs;
+};
+
+class ServerlessExportOutputs {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = options || {};
+    this.serviceName = serverless.service.getServiceName();
+    this.region = serverless.getProvider('aws').getRegion();
+    this.stage = serverless.getProvider('aws').getStage();
+    if (this.stage.startsWith('$')) this.stage = 'dev';
+    this.stackName = `${this.serviceName}-${this.stage}`;
+    this.config = serverless.service.custom.exportOutputs;
+    this.log = data => serverless.cli.consoleLog(data);
+
+    this.hooks = {
+      'after:deploy:deploy': this.export.bind(this),
+    };
+  }
+
+  getStackOutputs() {
+    return this.serverless
+      .getProvider('aws')
+      .request(
+        'CloudFormation',
+        'describeStacks',
+        { StackName: this.stackName },
+        this.stage,
+        this.region,
+      )
+      .then(response => {
+        const stack = response.Stacks[0];
+        const outputs = stack.Outputs || [];
+
+        return outputs.reduce(
+          (allOutputs, output) =>
+            Object.assign(allOutputs, {
+              [output.OutputKey]: output.OutputValue,
+            }),
+          {},
+        );
+      });
+  }
+
+  collectOutputs(outputs) {
+    if (!this.config) return outputs;
+
+    const isArray = obj =>
+      Object.prototype.toString.call(obj) === '[object Array]';
+    const isObject = obj =>
+      Object.prototype.toString.call(obj) === '[object Object]';
+
+    const targetOutputKeys = isArray(this.config)
+      ? this.config
+      : this.config.include || [];
+    const targetOutputs = {};
+
+    targetOutputKeys.forEach(entry => {
+      let key = entry;
+      let obj = outputs;
+      if (isObject(entry)) {
+        key = Object.keys(entry)[0];
+        obj = entry;
+      }
+      targetOutputs[key] = obj[key];
+    });
+
+    return targetOutputs;
+  }
+
+  processOutputs(outputs) {
+    if (this.config && this.config.handler) {
+      return this.config.handler(outputs, this.serverless, this.options);
+    }
+    return defaultHandler(outputs);
+  }
+
+  createFile(outputs) {
+    const path =
+      this.config && this.config.output && this.config.output.file
+        ? this.config.output.file
+        : defaultFile;
+    const targetFormat =
+      this.config && this.config.output && this.config.output.format
+        ? this.config.output.format
+        : defaultFormat;
+    let formattedOutputs = null;
+
+    switch (targetFormat.toLowerCase()) {
+      case 'toml':
+        formattedOutputs = tomlify.toToml(outputs);
+        break;
+      case 'yaml':
+      case 'yml':
+        formattedOutputs = yamljs.stringify(outputs);
+        break;
+      case 'json':
+        formattedOutputs = JSON.stringify(outputs);
+        break;
+      default:
+        throw new Error(`${targetFormat} is not supported`);
+    }
+
+    try {
+      fs.writeFileSync(path, formattedOutputs);
+    } catch (e) {
+      throw new Error(`Failed to create file: ${path}`);
+    }
+
+    return Promise.resolve();
+  }
+
+  export() {
+    return this.getStackOutputs()
+      .then(stackOutputs => this.collectOutputs(stackOutputs))
+      .then(targetOutputs => this.processOutputs(targetOutputs))
+      .then(exportOutputs => this.createFile(exportOutputs))
+      .catch(error => {
+        this.log(error);
+      });
+  }
+}
+
+module.exports = ServerlessExportOutputs;
